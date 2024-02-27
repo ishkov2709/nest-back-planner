b6bb424e6eba97169f86286e660320d42f67f144
ishkov2709 <dmitryishkov27@gmail.com>
2024-02-27 22:33:10 +0200

    add login service and register

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { User } from 'prisma/node_modules/.prisma/client';
+import { AuthDto } from 'src/auth/dto/auth.dto';
+import { hash } from 'argon2';
 // import { CreateUserDto } from './dto/create-user.dto';
 // import { UpdateUserDto } from './dto/update-user.dto';
 
@@ -16,6 +18,29 @@ export class UserService {
       include: {
         tasks: true,
       },
+    });
+  }
+
+  getByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+      include: {
+        tasks: true,
+      },
+    });
+  }
+
+  async create(dto: AuthDto) {
+    const user = {
+      email: dto.email,
+      name: '',
+      password: await hash(dto.password),
+    };
+
+    return this.prisma.user.create({
+      data: user,
     });
   }
 }
