a616903b650d68c6a2964fe97c8a336cef12afa4
ishkov2709 <dmitryishkov27@gmail.com>
2024-03-05 22:55:06 +0200

    add pomodoro timer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
 import { TimeBlockModule } from './time-block/time-block.module';
+import { PomodoroModule } from './pomodoro/pomodoro.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { TimeBlockModule } from './time-block/time-block.module';
     UserModule,
     TaskModule,
     TimeBlockModule,
+    PomodoroModule,
   ],
 })
 export class AppModule {}
